929e36ae7cc391d5e64e6b602b5179da75ea08a3
Daniel <daniel.adamov@student.howest.be>
2026-05-04 09:36:55 +0200

    Project Setup
    Created blueprint, edited Plugin Asset in editor, linked BT to Player.

---
+++ Plugins/GameFeatures/AdamovDanielZombie/Source/AdamovDanielZombieRuntime/StudentPerceptor.cpp
@@ -0,0 +1,23 @@
+﻿#include "StudentPerceptor.h"
+
+
+UStudentPerceptor::UStudentPerceptor()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UStudentPerceptor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (auto PerceptionComp = GetOwner()->GetComponentByClass<UAIPerceptionComponent>())
+	{
+		PerceptionComp->OnTargetPerceptionUpdated.AddDynamic(this, &UStudentPerceptor::OnPerceptionUpdated);
+	}
+}
+
+void UStudentPerceptor::OnPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
+{
+	GEngine->AddOnScreenDebugMessage(5, 1.f, FColor::Green, 
+	FString::Printf(TEXT("Saw Something!")));
+}
---
+++ Plugins/GameFeatures/AdamovDanielZombie/Source/AdamovDanielZombieRuntime/StudentPerceptor.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
+#include "Perception/AISenseConfig_Damage.h"
+#include "Perception/AISense_Damage.h"
+#include "StudentPerceptor.generated.h"
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class ADAMOVDANIELZOMBIERUNTIME_API UStudentPerceptor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UStudentPerceptor();
+	
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
+};
