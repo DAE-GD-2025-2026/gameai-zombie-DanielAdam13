0e7ddb4ed252baf556888a79013ef1d83a1c5cad
Daniel <daniel.adamov@student.howest.be>
2026-05-31 17:23:41 +0200

    Perception of Zombie, Item, House
    Started implementation of the "memorization" of already seen actors or actors that have damages the survivor.
    The OnPerceptionUpdated method checks the AISense type and calls HandleDamage or HandleSight depending on which type it is.
    -------------------------------------
    Created 3 structs with the base being PerceptedActor and the other being specific for an Item and House. These structs hold the memorizeable information for each type.
    -------------------------------------
    Handle Sight checks if the current actor is already in one of the TArrays for Zombies, Items or Houses and updates the visible, location and time flags as well as add it to the specific TArray is it is not in it YET.

--- Plugins/GameFeatures/AdamovDanielZombie/Source/AdamovDanielZombieRuntime/StudentPerceptorDanielAdamov.cpp
+++ Plugins/GameFeatures/AdamovDanielZombie/Source/AdamovDanielZombieRuntime/StudentPerceptorDanielAdamov.cpp
@@ -1,23 +1,143 @@
 ﻿#include "StudentPerceptorDanielAdamov.h"
 
+#include "Items/BaseItem.h"
+#include "Village/House/House.h"
+#include "Zombies/BaseZombie.h"
+
+// Names must match the BB keys in editor
+namespace SurvivorBBKeys
+{
+	static const FName ThreatActor { TEXT("ThreatActor") };
+	static const FName TargetItem { TEXT("TargetItem") };
+	static const FName KnownHouseTarget { TEXT("KnownHouseTarget") };
+}
 
 UStudentPerceptorDanielAdamov::UStudentPerceptorDanielAdamov()
 {
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false; // Timer driven, no per-frame logic
 }
 
 void UStudentPerceptorDanielAdamov::BeginPlay()
 {
 	Super::BeginPlay();
 	
-	if (auto PerceptionComp = GetOwner()->GetComponentByClass<UAIPerceptionComponent>())
+	if (const auto PerceptionComp = GetOwner()->GetComponentByClass<UAIPerceptionComponent>())
 	{
 		PerceptionComp->OnTargetPerceptionUpdated.AddDynamic(this, &UStudentPerceptorDanielAdamov::OnPerceptionUpdated);
 	}
+	
+	// Start by refreshing memory
+	GetWorld()->GetTimerManager().SetTimer(
+		RefreshTimer, this, &UStudentPerceptorDanielAdamov::RefreshWorldMemory, RefreshInterval, true);
+}
+
+void UStudentPerceptorDanielAdamov::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	if (const UWorld* World = GetWorld())
+	{
+		World->GetTimerManager().ClearTimer(RefreshTimer);
+	}
+	Super::EndPlay( EndPlayReason );
 }
 
 void UStudentPerceptorDanielAdamov::OnPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
 {
-	GEngine->AddOnScreenDebugMessage(5, 1.f, FColor::Green, 
+	if (!Actor)
+		return;
+	
+	// Damage 
+	if (Stimulus.Type == UAISense::GetSenseID<UAISense_Damage>())
+	{
+		HandleDamage( Actor, Stimulus );
+		
+		GEngine->AddOnScreenDebugMessage(5, 1.f, FColor::Red, 
+	FString::Printf(TEXT("Hit Something!")));
+	}
+	else // Sight
+	{
+		HandleSight( Actor, Stimulus );
+		
+		GEngine->AddOnScreenDebugMessage(5, 1.f, FColor::Green, 
 	FString::Printf(TEXT("Saw Something!")));
+	}
+	
+	// REACT to new sightings now at this frame
+	WriteBlackboard();
+}
+
+void UStudentPerceptorDanielAdamov::HandleSight(AActor* Actor, const FAIStimulus& Stimulus)
+{
+	const bool bSensed{ Stimulus.WasSuccessfullySensed() };
+	const float TimeNow{ static_cast<float>(GetWorld()->GetTimeSeconds()) };
+	
+	// If actor is zombie -> save info
+	if (Cast<ABaseZombie>( Actor ))
+	{
+		FPerceivedActor* Record{ FindRecord( Zombies, Actor ) };
+		// If no record of zombie-> populate the Record and push to TArray
+		if (!Record)
+		{
+			Record = &Zombies.AddDefaulted_GetRef();
+			Record->Actor = Actor;
+		}
+		
+		Record->bIsVisible = bSensed;
+		Record->LastKnownLocation = bSensed ? Actor->GetActorLocation() : Stimulus.StimulusLocation;
+		Record->LastSeenTime = TimeNow;
+		
+		return;
+	}
+	
+	// If actor is an Item
+	if (ABaseItem* Item = Cast<ABaseItem>(Actor))
+	{
+		// Don't remember Garbage !!!
+		if ( Item->GetItemType() == EItemType::Garbage)
+			return;
+		
+		FPerceivedItem* Record{ FindRecord( Items, Actor ) };
+		// If no record of item-> populate the Record and push to TArray
+		if (!Record)
+		{
+			Record = &Items.AddDefaulted_GetRef();
+			Record->Actor = Actor;
+		}
+		
+		Record->Type = Item->GetItemType(); // -- Unique for Item --
+		Record->bIsVisible = bSensed;
+		Record->LastKnownLocation = Item->GetActorLocation();
+		Record->LastSeenTime = TimeNow;
+		
+		return;
+	}
+
+	// If actor is a House
+	if (Cast<AHouse>(Actor))
+	{
+		FPerceivedHouse* Record{ FindRecord(Houses, Actor) };
+		// If no record of House-> populate the Record and push to TArray
+		if (!Record)
+		{
+			Record = &Houses.AddDefaulted_GetRef();
+			Record->Actor = Actor;
+		}
+		
+		Record->bIsVisible = bSensed;
+		Record->LastKnownLocation = Actor->GetActorLocation();
+		Record->LastSeenTime = TimeNow;
+		
+		return;
+	}
+}
+
+void UStudentPerceptorDanielAdamov::HandleDamage(AActor* Actor, const FAIStimulus& Stimulus)
+{
+}
+
+void UStudentPerceptorDanielAdamov::RefreshWorldMemory()
+{
+}
+
+void UStudentPerceptorDanielAdamov::WriteBlackboard()
+{
 }
--- Plugins/GameFeatures/AdamovDanielZombie/Source/AdamovDanielZombieRuntime/StudentPerceptorDanielAdamov.h
+++ Plugins/GameFeatures/AdamovDanielZombie/Source/AdamovDanielZombieRuntime/StudentPerceptorDanielAdamov.h
@@ -2,11 +2,35 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Items/ItemType.h"
 #include "Perception/AIPerceptionComponent.h"
 #include "Perception/AISenseConfig_Sight.h"
 #include "Perception/AISenseConfig_Damage.h"
 #include "Perception/AISense_Damage.h"
 #include "StudentPerceptorDanielAdamov.generated.h"
+
+enum class EItemType : uint8;
+
+// ----------- Remembered Targets Structs -----------
+// For zombie actors
+struct FPerceivedActor
+{
+	TWeakObjectPtr<AActor> Actor;
+	FVector LastKnownLocation{ FVector::ZeroVector };
+	float LastSeenTime{ 0.f };
+	bool bIsVisible{ false };
+};
+// For items
+struct FPerceivedItem : FPerceivedActor
+{
+	EItemType Type{ EItemType::Garbage };
+};
+// For houses
+struct FPerceivedHouse : FPerceivedActor
+{
+	bool bVisited{ false };
+};
+// ------------------------------------------------
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class ADAMOVDANIELZOMBIERUNTIME_API UStudentPerceptorDanielAdamov : public UActorComponent
@@ -14,11 +38,57 @@ class ADAMOVDANIELZOMBIERUNTIME_API UStudentPerceptorDanielAdamov : public UActo
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this component's properties
 	UStudentPerceptorDanielAdamov();
 	
 	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override; // Just clears the refresh timer
 
 	UFUNCTION()
 	virtual void OnPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
+	
+protected:
+	UPROPERTY(EditAnywhere, Category = "Perceptor")
+	float RefreshInterval{ 0.25f };
+	
+	// How long a zombie survivor no longer sees stays in "memory"
+	UPROPERTY(EditAnywhere, Category = "Perceptor")
+	float ThreatMemoryDuration{ 5.0f };
+	
+	// Distance at which survivor consider house checked
+	UPROPERTY(EditAnywhere, Category = "Perceptor")
+	float HouseVisitedRange{ 350.0f };
+	
+private:
+	TArray<FPerceivedActor> Zombies;
+	TArray<FPerceivedItem> Items;
+	TArray<FPerceivedHouse> Houses;
+	
+	FTimerHandle RefreshTimer;
+	
+	void HandleSight(AActor* Actor, const FAIStimulus& Stimulus);
+	void HandleDamage(AActor* Actor, const FAIStimulus& Stimulus);
+	
+	void RefreshWorldMemory(); // Using thr Refresh Timer, updates the arrays
+	void WriteBlackboard();
+	
+	// Templated function so we can use it for a Zombie, Item and House
+	// Checks if an actor of a type already exists in the TArray
+	// TRecord is of type FPerceivedActor or derived
+	template<typename TRecord>
+	TRecord* FindRecord(TArray<TRecord>& Container, const AActor* Key) const;
 };
+
+template <typename TRecord>
+TRecord* UStudentPerceptorDanielAdamov::FindRecord(TArray<TRecord>& Container, const AActor* Key) const
+{
+	// TRecord is of FPerceivedActor or derived structs type
+	for (TRecord& R : Container)
+	{
+		// FPerceivedActor.Actor.Get()
+		if (R.Actor.Get() == Key)
+		{
+			return &R;
+		}
+	}
+	return nullptr;
+}
